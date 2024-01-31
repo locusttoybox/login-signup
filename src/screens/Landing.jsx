9a069547533695ee0e055069ad153c0c0c6a6601
Veronika Užāne <info@veronika-uzane.com>
2024-01-31 10:08:20 +0200

    new code

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, AppBar, Toolbar } from '@mui/material';
 
 import Title from '../components/typography';
 
 const Landing = () => {
     return (
         <Container>
-            <Grid conatiner className='title' item sm={12} md={6} style={{ justifyContent: 'center' }}>
-                <Title text='Welcome to your nameday app' size='h1' />
+            <Grid container>
+                <div className="title">
+                    <Title text='Welcome to your namday app' size='h1' />   
+                </div>
+                <div className="subtitle">
+                    <Title text='Some text here' size='h4'/>
+                </div>
             </Grid>
-            <Grid container className='subtile' item sm={12} md={6}>
 
-            </Grid>
+
+
         </Container>
 
-    )
-}
+
+        // <AppBar position='static'>
+        //     <Toolbar>
+        //         <Title text='Welcome' size='h4'/>
+        //     </Toolbar>
+        // </AppBar>
+    );
+};
 
 export default Landing;
