559157c1e40c98f4d238f9ca328cb30162de69e4
Veronika Užāne <info@veronika-uzane.com>
2024-01-31 15:12:47 +0200

    small changes to button styles and layout tuning css

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -8,10 +8,7 @@ const StyledButton = styled(MButton)(({ theme, color }) => ({
   fontSize: 16,
   width: '220px',
   marginBottom: '20px',
-  '&:before' : {
-    backgroundColor: theme.palette[color],
-    color: '#1B4242',
-  },
+  backgroundColor: '#1B4242',
   '&:hover': {
     backgroundColor: theme.palette[color],
     backgroundColor: '#1B4242',
