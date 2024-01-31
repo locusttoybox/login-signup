8b6ae604fa15c2f4fd8483cb2cca5279ce669e06
Veronika Užāne <info@veronika-uzane.com>
2024-01-31 12:08:43 +0200

    working on navbar styles

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -4,11 +4,17 @@ import '../index.css';
 
 import Title from '../components/typography';
 import Button from '../components/button';
+import Navbar from '../components/navbar';
 
 
 const Landing = () => {
+    const navTitle = <Title text='Welcome' size='h4'/>
+
     return (
         <Container>
+             <AppBar position='static'>
+                <Navbar title={navTitle}/>
+             </AppBar>
             <Grid container alignItems='center' height='100vh'>
                 <Grid item sx={12} md={8}>
                  <div className="container">
@@ -19,17 +25,10 @@ const Landing = () => {
                         <Title text='Short discription about app and features' size='h3' />
                     </div>
                 </div>
-                <Button text='Enter' style={{ marginTop: '20px' }}/>
+                <Button text='Enter' />
                 </Grid>
             </Grid>
         </Container>
-
-
-        // <AppBar position='static'>
-        //     <Toolbar>
-        //         <Title text='Welcome' size='h4'/>
-        //     </Toolbar>
-        // </AppBar>
     );
 };
 
