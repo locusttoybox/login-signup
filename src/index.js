3ff594da5d85132e7a0a53bbe321a01ebf1ca8a7
Veronika Užāne <info@veronika-uzane.com>
2024-02-07 10:21:36 +0200

    added react router dom, styling form and error page file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+      <RouterProvider router={router} >
+        <App />
+      </RouterProvider>
 
   </React.StrictMode>
 );
