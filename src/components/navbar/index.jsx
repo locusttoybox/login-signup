4aee472544198bc660a314846ff1e2ab61dc989e
Veronika Užāne <info@veronika-uzane.com>
2024-02-14 15:24:51 +0200

    new appBar component

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,5 +1,10 @@
+import React from 'react';
 import { styled } from '@mui/material';
+import { AppBar, Box } from '@mui/material';
 import { Toolbar as MToolbar } from '@mui/material';
+
+import Button from '../button';
+import Link from '../link';
 
 const StyledToolbar = styled(MToolbar)(({ theme }) => ({
   backgroundColor: 'transparent',
@@ -12,4 +17,27 @@ const Navbar = ({ children, size }) => {
   return <StyledToolbar variant={size}>{children}</StyledToolbar>;
 };
 
-export default Navbar;
+const MyAppBar = () => {
+  return (
+    <AppBar
+      className="app-bar"
+      position="static"
+      style={{ background: 'transparent', boxShadow: 'none', width: '100%' }}
+    >
+      <Navbar>
+        <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+          <div className="nav-links">
+            <Link text="About" />
+            <Link text="Contact" />
+            <Link text="Something" />
+          </div>
+        </Box>
+        <Box className="login-landing">
+          <Button href="login" text="Login" small bgColor="light" />
+        </Box>
+      </Navbar>
+    </AppBar>
+  );
+};
+
+export default MyAppBar;
