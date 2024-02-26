cd008c0a5d62acdfd21bfdbc3c59e125f214f034
Veronika Užāne <info@veronika-uzane.com>
2024-02-26 15:11:21 +0200

    fixing imports

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Grid } from '@mui/material';
 import '../components/styles.css';
-import { loginUser } from '../features/couterSlice';
+import loginUser from '../features/counter/couterSlice';
 
 import userIcon from '../assets/user.png';
 import passwordIcon from '../assets/padlock.png';
