19c012b5ec0f49be89e6d3077fa46bb1455a1402
Veronika Užāne <info@veronika-uzane.com>
2024-02-27 15:44:45 +0200

    deleted

--- src/components/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userSlice from '../../features/counter/counterSlice';
-
-export default configureStore({
-  reducer: {
-    counter: userSlice,
-  },
-});
