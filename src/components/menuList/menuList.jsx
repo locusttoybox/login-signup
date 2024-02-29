f07922d486993257d508ee2821d3834cdb66f7b4
Veronika Užāne <info@veronika-uzane.com>
2024-02-29 11:48:48 +0200

    working on layout

--- src/components/menuList/menuList.jsx
+++ src/components/menuList/menuList.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { styled } from '@mui/material';
 import { MenuList as MMenuList } from '@mui/material';
+import PropTypes from 'prop-types';
 
 const StyledMenuList = styled(MMenuList)(({ theme }) => ({
   color: theme.palette.common,
 }));
 
-const MenuList = () => {
-  return <StyledMenuList></StyledMenuList>;
+const MenuList = ({ text }) => {
+  return <StyledMenuList text={text}></StyledMenuList>;
+};
+
+MenuList.propTypes = {
+  text: PropTypes.string,
 };
 
 export default MenuList;
--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import { Grid } from '@mui/material';
-import Title from '../components/typography';
+import { Grid, List, ListItem, ListItemText, Typography } from '@mui/material';
+// import Title from '../components/typography';
 import MyAppBar from '../components/navbar';
 import Page from '../components/page';
 
 const Dashboard = () => {
   return (
     <Page AppBarComponent={MyAppBar}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={3} style={{ padding: '0' }}>
-          <Title text="something" />
+      <div className="container-dashboard">
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={3}>
+            <List>
+              <ListItem button>
+                <ListItemText primary="Something" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Something" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Something" />
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <Typography variant="h2">Something</Typography>
+            {/* Add more content here */}
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={9} style={{ padding: '0' }}>
-          <Title text="something" />
-        </Grid>
-        <Grid item xs={12} md={3} style={{ padding: '0' }}>
-          <Title text="something" />
-        </Grid>
-      </Grid>
+      </div>
     </Page>
   );
 };
